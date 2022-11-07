2aa4ebdf947dfd5577a1717244de6789ef69e169
OxyRRR <ksenijarepina230488@gmail.com>
2022-11-07 17:50:30 +0500

    Initial commit

---
+++ app/src/main/java/com/example/design_task/LoginFragment.kt
@@ -0,0 +1,25 @@
+package com.example.design_task
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
